aad7b32e350ade7afbe86d05b495d64ae48b8ba6
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2025-12-22 17:09:56 +0500

    loops done

---
+++ frontend/javascript/04-loops.js
@@ -0,0 +1,18 @@
+// FOR LOOP
+for(let i=1; i<10 ; i++ ){
+    console.log(i);
+}
+
+// WHILE LOOP
+let m=1;
+while(m<=20){
+    console.log(m);
+    m++;
+}
+
+// D0-WHILE LOOP
+let n=0;
+do{
+    console.log(n);
+    n++;
+}while(n<10);
